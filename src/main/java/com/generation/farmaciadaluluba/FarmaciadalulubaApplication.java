b1e79d2060160b219444e9ac313ae646f82c64dc
unknown <luiza.kormann@gmail.com>
2024-01-04 11:27:28 -0300

    alteração versão spring 3.2.0

---
+++ src/main/java/com/generation/farmaciadaluluba/FarmaciadalulubaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciadaluluba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciadalulubaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciadalulubaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmaciadaluluba/FarmaciadalulubaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciadaluluba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciadalulubaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
